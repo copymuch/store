0985c689a1559228deeffc4e3ae58d053ad36e87
Mahnev <Mahnev.web@gmail.com>
2022-09-06 00:49:37 +0700

    script_update_vue-list-1.5

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let arrows = document.querySelectorAll('.p_arrow');
 let title1 = document.querySelector('h1');
 let img = document.querySelector('.img_js');
 let counter = 1;
+let counter2 = 0;
 let imgSrc = img.getAttribute('src');
 let pop = document.querySelector('.pop');
 let items = document.querySelectorAll('.items_item');
@@ -11,11 +12,15 @@ let gl = document.querySelector('.global');
 let body = document.querySelector('body');
 let burger = document.querySelector('.burger');
 let header = document.querySelector('.header_nav_block');
+let card = document.querySelector('.pop_card');
+let message = document.querySelector('.message');
+let shopCounter = document.querySelector('.shopCounter');
 
 pop.style.display = 'none';
 bgcolor.style.backgroundColor = '#DCE5E2';
 gl.style.display = 'none';
 body.style.overflowY = 'visible';
+message.style.display = 'none';
 
 arrows.forEach(function(arrow){
 	arrow.onclick = function(){
@@ -46,8 +51,15 @@ close.onclick = function(){
 	pop.style.display = 'none';
 	body.style.overflowY = 'visible';
 	gl.style.display = 'none';
+	message.style.display = 'none';
 }
 
 burger.onclick = function(){
 	burger.classList.toggle('burger_move');
+}
+
+card.onclick = function(){
+	message.style.display = 'block';
+	counter2++;
+	shopCounter.textContent = counter2;
 }
